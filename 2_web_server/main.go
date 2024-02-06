bd72f26fe6a337ca04959017ad1885dc10ba8d1d
Carlos Mena <50986837+carlosm00@users.noreply.github.com>
2024-02-06 21:32:32 +0100

    updated ref order

--- 2_web_server/main.go
+++ 2_web_server/main.go
@@ -6,12 +6,12 @@ import (
     "net/http"
 )
 
-// Ref. 3
+// Ref. 2
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Your Golang-based Web Server is working properly!")
 }
 
-// Ref. 2
+// Ref. 3
 func main() {
     http.HandleFunc("/", handler)
     fmt.Println("Server listening on port 8080")
